0bc1c9c2e869886241634f5de69bb41882c0f54a
kevinzogu <kevinzogu@gmail.com>
2025-03-29 10:22:28 +0100

    first commit

---
+++ CofeeShopManagement/src/main/java/al/sda/CofeeShopManagement/CofeeShopManagementApplication.java
@@ -0,0 +1,13 @@
+package al.sda.CofeeShopManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofeeShopManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofeeShopManagementApplication.class, args);
+	}
+
+}
---
+++ CofeeShopManagement/src/test/java/al/sda/CofeeShopManagement/CofeeShopManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package al.sda.CofeeShopManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofeeShopManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
